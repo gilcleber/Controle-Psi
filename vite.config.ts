57b99723df7303bde7d736c5f68d334cf45ed7ab
gilcleber <gilcleberlocutor@gmail.com>
2026-06-12 18:58:03 -0300

    fix: corrige base url do vite para vercel em vez do gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
-  base: '/Controle-Psi/',
+  base: '/',
   plugins: [
     react(),
     VitePWA({
