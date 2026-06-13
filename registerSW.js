717042ea4dc65410f08366896437f9c7a594472a
gilcleber <gilcleberlocutor@gmail.com>
2026-06-13 12:45:09 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Controle-Psi/sw.js', { scope: '/Controle-Psi/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
